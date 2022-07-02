46de3af8675ad9270a1790d165234a64f4ff0a6d
som <somsinghlodhi2323@gmail.com>
2022-07-02 12:28:57 +0530

    template code finished

---
+++ header_file.h
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+using namespace std;
+class node {
+    public :
+    int data ;
+    node * left_child ;
+    node * right_child ;
+};
